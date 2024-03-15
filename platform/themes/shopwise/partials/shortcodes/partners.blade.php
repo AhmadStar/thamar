b4b3b4320eafcbbc48db81955b29356ce363f1d0
abdulhamid-ta <abdulhamid@mews.me>
2024-03-15 22:16:19 +0200

    update

--- platform/themes/shopwise/partials/shortcodes/partners.blade.php
+++ platform/themes/shopwise/partials/shortcodes/partners.blade.php
@@ -2,6 +2,11 @@
 <!-- Brand Area -->
 <div class="brand-area-two ptb-100" dir="ltr">
     <div class="container">
+        <div class="section-title text-center">
+
+            <h2>{{__('Partners')}}</h2>
+            {{--<span class="sp-color2">{{$shortcode->content_1}}</span>--}}
+        </div>
         <div class="brand-slider owl-carousel owl-theme">
             @foreach ($partners as $partner)
             <div class="brand-item">
--- platform/themes/shopwise/views/ecommerce/customers/register.blade.php
+++ platform/themes/shopwise/views/ecommerce/customers/register.blade.php
@@ -1,5 +1,30 @@
-@php Theme::set('pageName', __('Sign up')) @endphp
+{{--@php Theme::set('pageName', __('Sign up')) @endphp--}}
+@php
+app()->getLocale() == 'ar' ? ($dir = 'left') : ($dir = 'right');
+$lang = app()->getLocale();
+@endphp
 
+
+        <!-- Inner Banner -->
+<div class="inner-banner">
+    <div class="container">
+        <div class="inner-title text-center">
+            <h3>{{ __("register") }}</h3>
+            <ul>
+                <li>
+                    <a href="/{{$lang}}">{{ __("Home")}}</a>
+                </li>
+                <li>
+                    <i class='bx bx-chevrons-right'></i>
+                </li>
+                <li>{{ __('register') }}</li>
+            </ul>
+        </div>
+    </div>
+    <div class="inner-shape">
+        <img src="https://new.jusoortraining.com/storage/general/effect-team-1.png" alt="">
+    </div>
+</div>
 <div class="login_register_wrap section">
     <div class="container">
         {!!
