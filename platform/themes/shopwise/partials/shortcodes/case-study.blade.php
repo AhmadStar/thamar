077511f2f5cf9d7c923326b37e4906084551163e
abdulhamid-ta <abdulhamid@mews.me>
2024-03-15 11:37:59 +0200

    .

--- platform/themes/shopwise/partials/shortcodes/case-study.blade.php
+++ platform/themes/shopwise/partials/shortcodes/case-study.blade.php
@@ -23,75 +23,9 @@
                     </div>
                 </div>
             @endforeach
-            <div class="case-study-item">
-                <a href="case-details.html">
-                    <img src="themes/shopwise/assets/images/case-study/case-study2.jpg" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="case-details.html">Business Solution</a></h3>
-                    <ul>
-                        <li><a href="case-details.html">Business</a></li>
-                        <li><a href="case-details.html">Planing</a></li>
-                    </ul>
-                    <a href="case-details.html" class="more-btn"><i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
 
-            <div class="case-study-item">
-                <a href="case-details.html">
-                    <img src="themes/shopwise/assets/images/case-study/case-study3.jpg" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="case-details.html">Business Solution</a></h3>
-                    <ul>
-                        <li><a href="case-details.html">Business</a></li>
-                        <li><a href="case-details.html">Planing</a></li>
-                    </ul>
-                    <a href="case-details.html" class="more-btn"><i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
 
-            <div class="case-study-item">
-                <a href="case-details.html">
-                    <img src="themes/shopwise/assets/images/case-study/case-study4.jpg" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="case-details.html">UI/UX Research</a></h3>
-                    <ul>
-                        <li><a href="case-details.html">UI/UX Design</a></li>
-                        <li><a href="case-details.html">Research</a></li>
-                    </ul>
-                    <a href="case-details.html" class="more-btn"><i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
 
-            <div class="case-study-item">
-                <a href="case-details.html">
-                    <img src="themes/shopwise/assets/images/case-study/case-study6.jpg" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="case-details.html">Business Solution</a></h3>
-                    <ul>
-                        <li><a href="case-details.html">Solution</a></li>
-                        <li><a href="case-details.html">Business</a></li>
-                    </ul>
-                    <a href="case-details.html" class="more-btn"><i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
-
-            <div class="case-study-item">
-                <a href="case-details.html">
-                    <img src="themes/shopwise/assets/images/case-study/case-study2.jpg" alt="Images">
-                </a>
-                <div class="content">
-                    <h3><a href="case-details.html">Web Development</a></h3>
-                    <ul>
-                        <li><a href="case-details.html">Design</a></li>
-                        <li><a href="case-details.html">Development</a></li>
-                    </ul>
-                    <a href="case-details.html" class="more-btn"><i class='bx bx-right-arrow-alt'></i></a>
-                </div>
-            </div>
         </div>
     </div>
 </div>
